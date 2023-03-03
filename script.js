7fc0f1b5b36a897136b2fce55c5115f3438dfaaf
Gustavo <gstvrrds@gmail.com>
2023-03-02 23:12:56 -0300

    adicionando funcão de remover item

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
+let produtos = document.querySelectorAll(".lista-produtos-single");
 document.querySelectorAll(".valorUnit").forEach((element) => {
-  valorTotal(element.innerHTML);
+  valorTotal(element.innerHTML, true);
+});
+
+produtos.forEach((element) => {
+  element.addEventListener("click", () => {
+    let valorDoProduto = element.querySelector(".valorUnit").innerHTML;
+    valorTotal(Number(valorDoProduto), false);
+    element.remove();
+  });
 });
 
 let items = [];
+
 let enviar = document.querySelector("input[type = submit]");
 enviar.addEventListener("click", () => {
   let nome = document.querySelector("input[name=nome_produto]").value;
   let valor = document.querySelector("input[name=valor_produto]").value;
   console.log(`
-    ${nome}
-    ${valor}
-    `);
+  ${nome}
+  ${valor}
+  `);
   items.push({
     nomeProduto: nome,
     valorProduto: valor,
   });
   adicionaNoDom(document.querySelector(".lista-produtos"));
-  valorTotal(Number(items[items.length - 1].valorProduto));
+  valorTotal(Number(items[items.length - 1].valorProduto), true);
 });
 
-
-function valorTotal(valorNovo) {
+function valorTotal(valorNovo, soma) {
+  console.log(soma);
   let valorTotal = document.querySelector(".valorTotal");
-  valorTotal.innerHTML = Number(valorTotal.innerHTML) + Number(valorNovo);
+  if (soma) {
+    valorTotal.innerHTML = Number(valorTotal.innerHTML) + Number(valorNovo);
+  } else {
+    valorTotal.innerHTML = Number(valorTotal.innerHTML) - Number(valorNovo);
+  }
 }
 
 function adicionaNoDom(itens) {
@@ -33,4 +47,3 @@ function adicionaNoDom(itens) {
   }</span></h3>
 </div>`;
 }
-
